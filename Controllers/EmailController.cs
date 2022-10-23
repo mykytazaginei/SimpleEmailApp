7fa2a4047e6bf385917b7cfa8d3967999dd31ec1
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-10-23 15:31:48 +0200

    implement SendEmail() method

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        public IActionResult SendEmail(string body)
+        {
+
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("luther53@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("luther53@ethereal.email"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smpt = new SmtpClient();
+            smpt.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smpt.Authenticate("luther53@ethereal.email", "26evysmwTXGAbpfWwe");
+            smpt.Send(email);
+            smpt.Disconnect(true);
+
+        }
+    }
+}
